d9aa2b37d061c536f6d9aeba4eb67596590a8131
Kevin <kevpadilla01@gmail.com>
2025-08-16 19:39:25 -0300

    home page updated

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,16 +1,26 @@
 import { RippleButton } from "@/components/animate-ui/buttons/ripple";
+import { SignedIn, SignedOut } from "@clerk/clerk-react";
 import { Link } from "react-router";
 
 const Home = () => {
   return (
     <section>
-      <div className="flex flex-col items-center justify-center gap-10 pt-40 border">
+      <div className="flex flex-col items-center justify-center gap-10 pt-40">
         <h1 className="text-6xl text-center text-balance">
           Bkmarky: Minimalist bookmark manager
         </h1>
-        <Link to={"/login"}>
-          <RippleButton>Get started</RippleButton>
-        </Link>
+        <div>
+          <SignedIn>
+            <Link to={"/bookmarks"}>
+              <RippleButton>Go to my Bookmarks</RippleButton>
+            </Link>
+          </SignedIn>
+          <SignedOut>
+            <Link to={"/login"}>
+              <RippleButton>Get started</RippleButton>
+            </Link>
+          </SignedOut>
+        </div>
       </div>
     </section>
   );
